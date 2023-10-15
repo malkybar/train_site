a472517a6e539c572d2ed7e4159bbf4f550dfc16
malkybar <40530828@live.napier.ac.uk>
2023-10-15 16:49:26 +0100

    hello world commit

--- server.py
+++ server.py
@@ -1,14 +1,9 @@
-from  flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from  flask import Flask
 app = Flask(__name__)
-cors = CORS(app)
-app.config['Cors_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def hello():
      return "hello world"
- 
-@app.route("/trains", methods=["GET", "POST"])
-def trains():
-    endpoint = 'api.rtt.io/api/v1/json/search/DRM'
-    return jsonify(request.args.get(endpoint))
+
+if __name__ == "__main__":
+     app.run(host='0.0.0.0', debug=True)
