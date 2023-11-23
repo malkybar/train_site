78855b47f9454e8d6042347941e70323274593e6
malkybar <40530828@live.napier.ac.uk>
2023-11-23 11:46:43 +0000

    added sqlite3 code

--- server.py
+++ server.py
@@ -1,12 +1,32 @@
-from  flask import Flask, request, jsonify, render_template
-import requests
-import json
+from  flask import Flask, request, jsonify, render_template, g
+import requests, json, sqlite3
 from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
 USERNAME = 'rttapi_malcybar'
 PASSWORD = '88128c2486220ad3b31c9d5e3d61a6d08a2bc0db'
+
+db_location = 'var/database.db'
+def get_db():
+     db = getattr(g, 'db', None)
+     if db is None:
+          db = sqlite3.connect(db_location)
+          g.db = db
+     return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+     db = getattr(g, 'db', None)
+     if db is not None:
+          db.close()
+          
+def init_db():
+     with app.app_context():
+          db = get_db()
+          with app.open_resource('schema.sql', mode='r') as f:
+               db.cursor().executescript(f.read())
+          db.commit()
 
 @app.route("/")
 def home():
@@ -59,7 +79,21 @@ def live():
 
 @app.route("/saved_stations")
 def saved():
-     return render_template("saved_stations.html"), 200
+     db = get_db()
+     db.cursor().execute('insert into user values ("1", "hello@world.com", "1234")')
+     db.commit()
+     
+     page = []
+     page.append('<html><ul>')
+     sql = "SELECT * FROM user"
+     for row in db.cursor().execute(sql):
+          page.append('<li>')
+          page.append(str(row))
+          page.append('</li>')
+     
+     page.append('</ul></html>')
+     return ''.join(page)
+     #return render_template("saved_stations.html"), 200
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', debug=True)
